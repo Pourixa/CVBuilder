26b3a624a25566cdca848db1a70f4d3990aebf42
Pourixa <pouria.footballer@gmail.com>
2026-05-28 12:42:23 +0200

    init commit

---
+++ components/page.jsx
@@ -0,0 +1,20 @@
+
+
+function Name()
+{
+    return <h1>
+        Stark Terry
+    </h1>
+}
+
+
+
+
+function CVPage()
+{
+    return <div>
+        <Name />
+    </div>
+}
+
+export {CVPage}
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { CVPage } from '../components/page'
+import './App.css'
+
+function App() {
+  return <>
+    <CVPage />
+  </>
+}
+
+export default App
