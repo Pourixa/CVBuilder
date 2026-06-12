40774109532e7c8a49e97090ec54c90b0cb8a544
Pourixa <pouria.footballer@gmail.com>
2026-06-12 10:44:13 +0200

    Add dropdown catergory

--- components/editExpereince.jsx
+++ components/editExpereince.jsx
@@ -1,5 +1,8 @@
 import { AddButton } from './addbutton'
 import { SingleExps } from './singleExp'
+import { Title } from './editTitle'
+import { useState } from 'react'
+
 const fields = [
   { label: 'Job', name: 'job', type: 'text', placeholder: 'Engineer' },
   { label: 'Company', name: 'company', type: 'text', placeholder: 'Microsoft' },
@@ -8,12 +11,19 @@ const fields = [
 ]
 
 function JobForm({ experiences, onExperienceChange , onExperienceAdd , onExperienceDelete}) {
-  return (
-    <div className="editExperience">
+    const [folded,setFolded] = useState(true);
+
+    function handleFold ()
+    {
+      setFolded(!folded);
+    }
+
+  return <>
+    <Title name = {"Experiences"} folded={folded} onToggle={handleFold}/>
+    {(!folded && <div className="editExperience " >
             <AddButton addFunction = {onExperienceAdd}/>
       <SingleExps fields={fields} experiences = {experiences} onExperienceChange = {onExperienceChange} onExperienceDelete = {onExperienceDelete}/>
-
-    </div>
-  )
+    </div>)}
+  </>
 }
 export { JobForm }
